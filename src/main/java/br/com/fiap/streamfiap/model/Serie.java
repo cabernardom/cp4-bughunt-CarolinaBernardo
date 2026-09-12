bc4674efea7541f559b570f38e4bdb6ff5ca99f5
Carolina Bernardo <carolinabernardomstudio@gmail.com>
2026-09-11 21:57:34 -0300

    fix: bug01 corrigir preco e construtor de serie

--- src/main/java/br/com/fiap/streamfiap/model/Serie.java
+++ src/main/java/br/com/fiap/streamfiap/model/Serie.java
@@ -12,11 +12,13 @@ public class Serie extends Conteudo implements Promocionavel {
 
     // cria a série com os dados recebidos
     public Serie(String titulo, String categoria, int duracaoMinutos, int classificacaoEtaria, int numeroTemporadas) {
+        super(titulo, categoria, duracaoMinutos, classificacaoEtaria, true);
         this.numeroTemporadas = numeroTemporadas;
     }
 
     // preço da série: 4.90 por temporada
-    public double calcularPrecoAluguel(double desconto) {
+    @Override
+    public double calcularPrecoAluguel() {
         return 4.90 * numeroTemporadas;
     }
 
