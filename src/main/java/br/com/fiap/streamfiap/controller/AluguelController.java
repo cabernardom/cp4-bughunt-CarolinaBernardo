a8953f156c9676a360d6e0b3609296dd9c4a91fe
Carolina Bernardo <carolinabernardomstudio@gmail.com>
2026-09-11 22:05:59 -0300

    fix: bug11 responder usuario inexistente

--- src/main/java/br/com/fiap/streamfiap/controller/AluguelController.java
+++ src/main/java/br/com/fiap/streamfiap/controller/AluguelController.java
@@ -2,6 +2,7 @@ package br.com.fiap.streamfiap.controller;
 
 import br.com.fiap.streamfiap.exception.ClassificacaoIndicativaException;
 import br.com.fiap.streamfiap.exception.ConteudoNaoEncontradoException;
+import br.com.fiap.streamfiap.exception.UsuarioNaoEncontradoException;
 import br.com.fiap.streamfiap.model.Conteudo;
 import br.com.fiap.streamfiap.model.Usuario;
 import br.com.fiap.streamfiap.repository.ConteudoRepository;
@@ -25,7 +26,7 @@ public class AluguelController {
     public ResponseEntity<Usuario> alugar(@RequestParam Long usuarioId, @RequestParam Long conteudoId)
             throws ClassificacaoIndicativaException {
         Usuario usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado: " + usuarioId));
+                .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado: " + usuarioId));
         Conteudo conteudo = conteudoRepository.findById(conteudoId)
                 .orElseThrow(() -> new ConteudoNaoEncontradoException("Conteúdo não encontrado: " + conteudoId));
 
